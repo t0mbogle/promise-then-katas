a6fc1235c92378c432e331376adcb73730c82d88
Romy Greenfield <rgreenfield@adaptavist.com>
2020-08-22 07:51:58 +0100

    Refactored solutions to async await in promise-katas-2

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -12,38 +12,38 @@ You can look at the fakeApi to see how the data is returned
 // 1 Create a function that uses the getData function to make a request to the "food" URL and returns
 // the data
 
-const food = () => {
-    return getData("food").then(({ data }) => data);
+const food = async () => {
+    const { data } = await getData("food");
+    return data;
 };
 
 // 2 Create a function that uses the getData function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order
 
-const cat = () => {
-    return getData("cats").then(({ data }) => data.cats.sort());
+const cat = async () => {
+    const { data } = await getData("cats");
+    return data.cats.sort();
 };
 
 // 3 Create a function that uses the getData function to make a request to the "dogs" URL and returns
 // the naughtiest dog
 
-const dog = () => {
-    return getData("dogs").then(({ data }) => data.dogs.reduce((acc, val) => {
+const dog = async () => {
+    const { data } = await getData("dogs")
+    return data.dogs.reduce((acc, val) => {
         if (acc.naughty > val.naughty) return acc;
         return val;
-    }));
+    });
 };
-
 
 // 4 Create a function that uses the getData function to make requests to the "jokes" URL and returns
 // the joke object
 
-const joke = () => Promise.all([
-    getData("jokes", "question"),
-    getData("jokes", "answer")
-]).then(([one, two]) => {
-    return { question: one.joke, answer: two.answer };
-});
-
+const joke = async () => {
+    const { joke } = await getData("jokes", "question");
+    const { answer } = await getData("jokes", "answer");
+    return { question: joke, answer: answer };
+};
 
 module.exports = {
     food,
