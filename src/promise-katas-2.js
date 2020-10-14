313b532718500113c6ae4693071c036cda059f4a
Romy Greenfield <rgreenfield@adaptavist.com>
2020-10-14 21:30:14 +0100

    Updated promis-katas-2 to use fetch instead of getData

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -19,32 +19,32 @@ fetch("joke", "question")
 
 */
 
-// 1 Create a function that uses the getData function to make a request to the "food" URL and returns
+// 1 Create a function that uses the fetch function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
 const food = () => {
-    return getData("food").then(({ data }) => data);
+    return fetch("food").then(({ data }) => data);
 };
 
-// 2 Create a function that uses the getData function to make a request to the "cats" URL and returns
+// 2 Create a function that uses the fetch function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
 const cat = () => {
-    return getData("cats").then(({ data }) => data.cats.sort());
+    return fetch("cats").then(({ data }) => data.cats.sort());
 };
 
-// 3 Create a function that uses the getData function to make a request to the "dogs" URL and returns
+// 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
 const dog = () => {
-    return getData("dogs").then(({ data }) => data.dogs.reduce((acc, val) => {
+    return fetch("dogs").then(({ data }) => data.dogs.reduce((acc, val) => {
         if (acc.naughty > val.naughty) return acc;
         return val;
     }));
 };
 
 
-// 4 Create a function that uses the getData function to make requests to the "jokes" URL and returns
+// 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
 // a joke object with the key of question and answer - expected return { 
 //     question: "Why did the scarecrow win the Nobel Prize?",
 //     answer: "Because he was out-standing in his field."
@@ -55,8 +55,8 @@ const dog = () => {
 //
 
 const joke = () => Promise.all([
-    getData("jokes", "question"),
-    getData("jokes", "answer")
+    fetch("jokes", "question"),
+    fetch("jokes", "answer")
 ]).then(([one, two]) => {
     return { question: one.joke, answer: two.answer };
 });
